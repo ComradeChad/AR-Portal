4945a3690f1a4aa0e778369093fd2d030b3de2af
Chad Hillary <chadhillary2013@gmail.com>
2019-02-03 22:38:41 -0600

    Basic Portal Implemented

---
+++ Assets/scripts/passPortal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class passPortal : MonoBehaviour {
+
+	public Material[] mats;
+
+	const int inside = (int) CompareFunction.NotEqual;
+	const int outside = (int) CompareFunction.Equal;
+
+	// Use this for initialization
+	void Start () {
+		setMaterialMasks(outside);
+	}// end Start
+
+	void OnTriggerStay(Collider other) {
+		if (other.name != "Main Camera")
+			return;
+
+		if (transform.position.z > other.transform.position.z)
+			setMaterialMasks(outside);
+		else
+			setMaterialMasks(inside);
+	
+	}// end onCollide
+
+	// setMaterialMasks
+	// helper function for onCollide
+	private void setMaterialMasks(int mask) {
+		foreach (var mat in mats) {
+			mat.SetInt ("_StencilTest", mask);
+		}
+	}// end setMaterialMasks
+
+
+	void onDestroy() {
+		setMaterialMasks (inside);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}// end Update
+}
---
+++ Assets/shaders/portalShader.shader
@@ -0,0 +1,18 @@
+﻿Shader "Custom/portalShader"
+{
+	SubShader
+	{
+
+		ZWrite off // Don't write to Z buffer
+		ColorMask 0 // Transparent shader
+
+		Stencil {
+			Ref 1
+			Pass replace
+		}
+
+		Pass
+		{
+		}
+	}
+}
